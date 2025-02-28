987115e868c323ff8952f87607ec480f284d280d
BhavaniM29 <manne.bhavani@gmail.com>
2025-02-28 13:12:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
+# app.py
 from flask import Flask
 import os
-from ddtrace import patch_all
-
-# Enable Datadog tracing for all supported libraries
-patch_all()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask! This is your first app running on AWS with Datadog APM."
+    return "Hello, Flask! This is your first app running on AWS."
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
